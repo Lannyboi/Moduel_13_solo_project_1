97e4bec553ed1c44aa65f9ab32f0124b63660728
lanny42069 <91437706+lanny42069@users.noreply.github.com>
2022-07-20 13:35:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return(
+    <div>
+      <img className="pfp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRb4BkZFLEFcTR7vspNSVIevc-h8RURATXwtA&usqp=CAU"/>
+      <header className="personal-info">
+        <h1 className="name">Landon Gray</h1>
+        <h3 className="job-title">Frontend Developer</h3>
+        <p className="website">LandonGray.website</p>
+        <div className="buttons">
+          <button>Email</button>
+          <button className="linkedin-btn">Linkedln</button>
+        </div>
+      </header>
+      <div className="information">
+        <h1>About</h1>
+        <p>I started Frontend development in 2021.
+           I studied on a website called <a href='https://scrimba.com/' target="_blank">Scrimba</a> which taught me the basic skills of HTML,
+          CSS, JS, and React!
+        </p>
+        <h1>Intrests</h1>
+        <ul>
+          <li>Coding (Obviously)</li>
+          <li>Valorant/League (Video games)</li>
+          <li>Playing music & lisening to music</li>
+          <li>Solving Rubik's cubes (My heighest is 9x9x9)</li>
+          <li>Playing sports 🏀 ⚾</li>
+        </ul>
+      </div>
+      <footer>
+        <div className='social-media'>
+          <a href="https://twitter.com/LANDONG52473404?t=YUFaEdSY3xsnrBD_AO9OtQ&s=09" target="_blank"><div className='twitter platform'></div></a>
+          <a href="https://www.facebook.com/landon.gray.3150"><div className='facebook platform' target="_blank"></div></a>
+          <a href="https://instagram.com/oh_hello_7?igshid=YmMyMTA2M2Y=" target="_blank"><div className='instagram platform'></div></a>
+          <a href="https://github.com/Lannyboi" target="_blank"><div className='github platform'></div></a>
+        </div>
+        </footer>
+    </div>
+  )
+}
