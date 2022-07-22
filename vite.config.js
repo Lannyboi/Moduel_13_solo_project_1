3c28e9d956f622e97993a4e6dc869a1b81c0f9ec
lanny42069 <91437706+lanny42069@users.noreply.github.com>
2022-07-21 21:47:55 -0700

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/-Moduel_13_solo_project_1/",
+  base: "/Moduel_13_solo_project_1/",
   plugins: [react()]
 })
